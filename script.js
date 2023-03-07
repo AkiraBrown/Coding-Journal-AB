0b8207d2787f8f447553f8a43b720d63376b62e7
Akira Watson-Brown <akirawatson-brown@new-host-8.home>
2023-03-07 14:29:55 -0500

    Css-Sidebar-Menu-AB

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+function openMenu() {
+  document.getElementById("sidebar-Menu").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeMenu() {
+  document.getElementById("sidebar-Menu").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+/*-------------------------------------------------------------------------------------------------- */
